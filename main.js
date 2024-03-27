55936972b418765e9bafa3ab60b3693a9832c1f8
Jaelson Pereira <86059311+jaelps@users.noreply.github.com>
2024-03-27 21:18:51 +0000

    correção ok

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ function capturaDados() {
   var primNum = Number(document.getElementById("n1").value);
   var segundNum = Number(document.getElementById("n2").value);
   var operador = document.getElementById("minMenu").value;
-  console.log(primNum);
-  console.log(segundNum);
-  console.log(operador);
 
   if (operador == "+") {
     result = primNum + segundNum;
